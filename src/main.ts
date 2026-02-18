59a72167b56446f05c5c64505d171614c63e7578
TaylorSambajon <tsambajon@ucdavis.edu>
2026-02-18 12:33:47 -0800

    added circle to test for mouse tracking

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,43 @@
+import { Application, Graphics } from 'pixi.js'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = new Application();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function setup() {
+
+  await app.init({ 
+
+    background: '#6d83c0', 
+    resizeTo: window,
+    resolution: window.devicePixelRatio || 1,
+    autoDensity: true
+
+  });
+
+  document.body.appendChild(app.canvas);
+
+  const almond = new Graphics().circle(0, 0, 50).fill('#c0c0c1'); //blue fill, maybe we might have to change this lmao
+
+  almond.x = app.screen.width / 2;
+  almond.y = app.screen.height / 2;
+  almond.pivot.set(0);
+
+  app.stage.addChild(almond);
+
+  app.ticker.add(() => {
+    
+    const mouse = app.renderer.events.pointer;
+    const speed = 0.1;
+
+    let nextX = almond.x += (mouse.x - almond.x) * speed;
+    let nextY = almond.y += (mouse.y - almond.y) * speed;
+
+    const radius = 200;
+
+    almond.x = Math.max(radius, Math.min(nextX, app.screen.width - radius));
+    almond.y = Math.max(radius, Math.min(nextY, app.screen.height - radius));
+
+  });
+}
+
+setup();
