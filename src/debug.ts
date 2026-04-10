42224a887d15920fa538921abe705682c6be144a
TaylorSambajon <tsambajon@ucdavis.edu>
2026-04-10 14:13:16 -0700

    added new buddy movement, created debug file and added draw slice and draw grid functions

---
+++ src/debug.ts
@@ -0,0 +1,51 @@
+import { Graphics } from 'pixi.js'
+
+/*
+ * Creates the region map that blocks out different sections of active areas
+ */
+export class RegionMap extends Graphics {
+    constructor() { //basic constructor
+        super();
+    }
+
+    //draws a slice in the region map and fills it in
+    public drawSlice(x: number, y: number, sDeg: number, eDeg: number, color: number){
+        let degree1 = sDeg * (Math.PI / 180);
+        let degree2 = eDeg * (Math.PI / 180);
+        this.clear();
+        console.log("Da degrees in question, start:", degree1, ", end: ", degree2);
+
+        //begin the slice and place it at the center of the screen
+        this.beginPath();
+        this.moveTo(x,y);
+
+        //create an arc according to origin, map lines for each of this length, from
+        //start position to end position
+        this.arc(
+            x, y, 400, 
+            degree1, degree2,
+            true
+        );
+
+        //fill in lines and region
+        this.lineTo(x, y);
+        this.fill({color, alpha: 0.3});
+        this.stroke({
+            width: 2,
+            color,
+            alpha: 0.8
+        });
+
+    }
+
+    public drawGrid(startX: number, startY: number, endX: number, endY: number, color: number){
+        this.clear();
+
+        this.beginPath();
+        this.moveTo(startX,startY);
+
+        this.lineTo(endX, endY);
+        this.fill({color, alpha: 0.1});
+    }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { 
           Assets, 
           Application,
-          FederatedPointerEvent } from 'pixi.js'
+          FederatedPointerEvent
+        } from 'pixi.js'
 import { Character } from './buddy.ts'
+import { RegionMap } from './debug.ts'
 import './style.css'
 
 const app = new Application();
@@ -20,60 +22,94 @@ async function setup() {
 
   document.body.appendChild(app.canvas);
 
+  //load almond buddy json and atlas map
   const daSheet = await Assets.load('/assets/buddy-sheet.json');
   console.log("LOADED: ", daSheet);
 
+  //establish central limits
   const x_axis = app.screen.width / 2;
   const y_axis = app.screen.height /2;
   const center = 70;
   const near = 200;
-  
+
+  //create buddy
   const almond = new Character(daSheet);
 
+  //Buddy positioning
   almond.scale.set(0.5);
   almond.x = x_axis;
   almond.y = y_axis;
 
+  //Buddy interaction area
   app.stage.addChild(almond);
   app.stage.eventMode = 'static';
   app.stage.hitArea = app.screen;
 
+  //place overlay on top of buddy
+  const region_overlay = new RegionMap();
+  app.stage.addChild(region_overlay);
+
+  //Buddy looking math
   app.stage.on('globalpointermove', (event: FederatedPointerEvent) => {
     const { x, y } = event.global;
     
-    if (x > x_axis) {
+    //DRAW DA DEBUGGER
+    region_overlay.drawSlice(almond.x, almond.y, 0, -20, 0xd3f0cc);
 
-      if (y < y_axis + center){
+    if (x > x_axis) { //greater x values mean right of the screen
+      
+      if (y > y_axis + center){ //bottom half of screen, upper limit to looking straight right
+
+        almond.buddyLook('right_down.png');
+
+      } else if (y > y_axis - center ){ //bottom limit to looking direct right
 
         if (x < x_axis + center){
           almond.buddyLook('right0.png');
-        } else if (x < x_axis + near){
+        } else if (x < x_axis + (near*1.5)){
           almond.buddyLook('right1.png');
         } else {
           almond.buddyLook('right2.png');
         }
 
-      } else {
-        almond.buddyLook('right_down.png');
+      } else if (y > y_axis - near){ //bottom limit to almond buddy up right1 range
+
+        almond.buddyLook('right_up.png');
+
+      } else { //if y is smaller than any of those values, almond buddy looks up all the way
+
+        almond.buddyLook('right_up2.png');
+
       }
 
     }
 
-    if (x < x_axis) {
+    if (x < x_axis) { //smaller x values mean left of the screen
 
-      if (y < y_axis + center){
+      if (y > y_axis + center){ //bottom limit to look down
 
-        if (x > x_axis - center){
+        almond.buddyLook('left_down.png');
+
+      } else if (y > y_axis - center){ //bottom limit to look straight left
+
+        if (x > x_axis - center){ //center limit for left
           almond.buddyLook('left0.png');
-        } else if (x > x_axis - near){
+        } else if (x > x_axis - (near*1.5)){ //"near" limit for left
           almond.buddyLook('left1.png');
-        } else {
+        } else { //everything else, may limit this point to create a "box" for AB
           almond.buddyLook('left2.png');
         }
 
-      } else {
-        almond.buddyLook('left_down.png');
+      } else if (y > y_axis - near){ //bottom limit to look up left1
+
+        almond.buddyLook('left_up.png');
+
+      } else { //if y is smaller than y_axis - near bounds then look up2
+
+        almond.buddyLook('left_up2.png');
+
       }
+
     }
 
   });
