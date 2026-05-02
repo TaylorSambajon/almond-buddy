fa78f05c50e33849aea7db6239fa7abccedd6892
TaylorSambajon <tsambajon@ucdavis.edu>
2026-05-02 15:26:54 -0700

    Refactored: MenuBar is now Sprite (from Graphics)

--- src/menu.ts
+++ src/menu.ts
@@ -5,55 +5,21 @@
  *  - slight hovering but no overt movements
  *  - ABOUT, WORK EXPERIENCE, PROJECTS, PORTFOLIO, EXTRAS
  **/
-import { Container, Graphics } from 'pixi.js'
+import { Container, Sprite, Spritesheet } from 'pixi.js'
 
-export class MenuBar extends Graphics{
-    constructor(){
+export class MenuBar extends Sprite{
+    private sprite: Sprite;
+    private sheet: Spritesheet;
+
+    constructor(sheet: Spritesheet){
         super();
+
+        this.sprite = new Sprite(sheet.textures['menu.png']);
+        this.sheet = sheet;
+        this.addChild(this.sprite);
     }
 
-    public drawBar(x: number, y: number, color: number){
-        this.clear();
-        console.log("Drawing da menu yeaaaaa");
+    //need to be able to draw the bar
 
-
-        this.arc(
-            x,y,150,
-            Math.PI, 0,
-            false
-        );
-
-        this.lineTo(x,y);
-        this.fill(
-            {color: color, alpha: 0.3}
-        )
-        this.strokeStyle = {
-            width: 10,
-            color: color,
-            cap: 'round', join: 'round',
-            alpha: 0.8
-        }
-    }
-}
-
-export function makeMenu(x: number, y: number, color: number){
-    const menu_container = new Container();
-    menu_container.label = 'Menu Bar';
-    const menu = new MenuBar();
-
-
-    menu_container.addChild(menu);
-    menu.drawBar(x, y, color)
-
-    return menu_container;
-}
-
-/*
-    Cleans up current menu graphics for a new menu.
-*/
-export function cleanMenu(container: Container){
-    if (container.parent)
-        container.parent.removeChild(container);
-
-    container.destroy({children: true});
+    //need to be able to place menu icons
 }
